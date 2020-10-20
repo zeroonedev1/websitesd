50dd03a5ed2588b45a48f1baebea562455fe1483
dholueffect <shubhamdholu@gmail.com>
2020-10-20 17:39:41 +0530

    Updates

---
+++ precache-manifest.6334aea31de5e1479150dd3361fb0cc7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a2fbad606c1781a6ce06b10c6a877e",
+    "url": "/websitesd/index.html"
+  },
+  {
+    "revision": "12cbb6ed7974f4b6bacc",
+    "url": "/websitesd/static/css/main.0ce21bf5.chunk.css"
+  },
+  {
+    "revision": "7be7846578f641fcba5a",
+    "url": "/websitesd/static/js/2.6263176c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/websitesd/static/js/2.6263176c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cbb6ed7974f4b6bacc",
+    "url": "/websitesd/static/js/main.6dd0b3ea.chunk.js"
+  },
+  {
+    "revision": "d5d8c8211740c47a6cdd",
+    "url": "/websitesd/static/js/runtime-main.6a870096.js"
+  },
+  {
+    "revision": "b44e892984bc4c46c7533e29781ace47",
+    "url": "/websitesd/static/media/Coldplay.b44e8929.jpg"
+  },
+  {
+    "revision": "f9ba2247df1f4d099687fc40110b6702",
+    "url": "/websitesd/static/media/Jayz.f9ba2247.jpg"
+  },
+  {
+    "revision": "453fbe54fcb8edebcfde8aa315fb2676",
+    "url": "/websitesd/static/media/LPjpg.453fbe54.jpg"
+  },
+  {
+    "revision": "6f9c7964731c39fd558862d8737d3351",
+    "url": "/websitesd/static/media/acdc.6f9c7964.jpg"
+  },
+  {
+    "revision": "8907316909d1e9fe40c1315a911da79b",
+    "url": "/websitesd/static/media/pic.89073169.png"
+  },
+  {
+    "revision": "16cbdf04273b950fe048ccbb9a86e0f4",
+    "url": "/websitesd/static/media/pinkfloyd.16cbdf04.jpg"
+  },
+  {
+    "revision": "53278ff24fbe993b17f9a235820686b9",
+    "url": "/websitesd/static/media/tupacjpg.53278ff2.jpg"
+  }
+]);
